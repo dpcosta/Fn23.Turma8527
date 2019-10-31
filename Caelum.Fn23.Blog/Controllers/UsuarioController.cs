ec66ccca1235bedb02f1b5e5e24969a41cdd88c8
Daniel Costa <dpcosta@gmail.com>
2019-10-31 17:03:05 -0300

    Rotas Login e Autentica; formulário de login.

---
+++ Caelum.Fn23.Blog/Controllers/UsuarioController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Caelum.Fn23.Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Caelum.Fn23.Blog.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Autentica(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", new { Controller = "Post", Area = "Admin" });
+            }
+            return View("Login", model);
+        }
+    }
+}
---
+++ Caelum.Fn23.Blog/Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Caelum.Fn23.Blog.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name ="Usuário")]
+        public string Login { get; set; }
+        [Required]
+        [Display(Name = "Senha")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
