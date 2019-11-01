5fbb7cd9aad2b6614703e0ac5d62123e281b6250
Daniel Costa <dpcosta@gmail.com>
2019-11-01 16:21:00 -0300

    Cadastro de usuários

--- Caelum.Fn23.Blog/Controllers/UsuarioController.cs
+++ Caelum.Fn23.Blog/Controllers/UsuarioController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using Caelum.Fn23.Blog.DAL;
+using Caelum.Fn23.Blog.Filtros;
 using Caelum.Fn23.Blog.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -22,11 +23,13 @@ namespace Caelum.Fn23.Blog.Controllers
             return View();
         }
 
+        [Route("logout")]
         public IActionResult Logout()
         {
             HttpContext.Session.Remove("usuario");
             return RedirectToAction("Login");
         }
+
         [Route("autentica")]
         public IActionResult Autentica(LoginViewModel model)
         {
@@ -35,13 +38,48 @@ namespace Caelum.Fn23.Blog.Controllers
                 var usuario = dao.Todos.Where(u => u.Nome == model.Login && u.Senha == model.Password).FirstOrDefault();
                 if (usuario != null)
                 {
-                    // colocar na sessão
                     HttpContext.Session.SetString("usuario", JsonConvert.SerializeObject(usuario));
                     return RedirectToAction("Index", new { Controller = "Post", Area = "Admin" });
                 }
                 ModelState.AddModelError("usuarioNaoEncontrado", "Usuário não encontrado");
             }
             return View("Login", model);
+        }
+
+        [AutorizacaoFilter]
+        public IActionResult Index()
+        {
+            return View(dao.Todos);
+        }
+
+        [AutorizacaoFilter]
+        public IActionResult Novo()
+        {
+            return View();
+        }
+
+        [AutorizacaoFilter]
+        public IActionResult Adiciona(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var usuario = new Usuario
+                {
+                    Nome = model.Login,
+                    Email = model.Email,
+                    Senha = model.Senha
+                };
+                dao.Incluir(usuario);
+                return RedirectToAction("Index", new { area = "", controller = "Usuario" });
+            }
+            return View("Novo", model);
+        }
+
+        [AutorizacaoFilter]
+        public IActionResult Excluir(Usuario model)
+        {
+            dao.Excluir(model);
+            return RedirectToAction("Index", new { area = "", controller = "Usuario" });
         }
     }
 }
---
+++ Caelum.Fn23.Blog/Models/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Caelum.Fn23.Blog.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Usuário", Prompt = "Digite seu nome de usuário")]
+        public string Login { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email", Prompt = "Digite seu endereço de email")]
+        public string Email { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Required]
+        [Compare("Senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmação de senha")]
+        public string ConfirmacaoSenha { get; set; }
+    }
+}
